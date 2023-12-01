8fc4392e0b481a88a340b26b7cb143e14d1dcf23
ThatOtherAndrew <stroev.andrew@gmail.com>
2023-12-01 08:37:56 +0000

    Add template

---
+++ template/part_2.py
@@ -0,0 +1,13 @@
+def function():
+    ...
+
+
+def main() -> None:
+    with open('input.txt') as file:
+        _____ = [line for line in file]
+
+    print('Day DAYNUMBER.2 Answer:', ...)
+
+
+if __name__ == '__main__':
+    main()

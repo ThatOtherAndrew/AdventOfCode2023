8fee6c3d391d99649493eaec3a52f2721f6096bc
ThatOtherAndrew <stroev.andrew@gmail.com>
2023-12-10 12:38:37 +0000

    🌟 Day 9

---
+++ 09/part_1.py
@@ -0,0 +1,25 @@
+from math import prod
+
+
+def lagrange_extrapolate(sequence: list[int]) -> float:
+    # https://math.libretexts.org/Courses/Angelo_State_University/Mathematical_Computing_with_Python/3%3A_Interpolation_and_Curve_Fitting/3.2%3A_Polynomial_Interpolation
+    return sum(
+        sequence[i] * prod(
+            (len(sequence) - j) / (i - j)
+            for j in range(len(sequence))
+            if i != j
+        )
+        for i in range(len(sequence))
+    )
+
+
+def main() -> None:
+    with open('input.txt') as file:
+        sequences = [list(map(int, line.split())) for line in file]
+
+    sum_predictions = round(sum(map(lagrange_extrapolate, sequences)))
+    print('Day 9.1 Answer:', sum_predictions)
+
+
+if __name__ == '__main__':
+    main()
